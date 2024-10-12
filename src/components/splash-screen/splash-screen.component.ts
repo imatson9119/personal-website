906dd179219d8175261d978e9add96959935af60
Ian <imatson9119@gmail.com>
2024-10-12 13:04:35 -0500

    ThreeJS checkpoint

--- src/components/splash-screen/splash-screen.component.ts
+++ src/components/splash-screen/splash-screen.component.ts
@@ -1,102 +1,99 @@
 import { html, LitElement } from 'lit';
 import { customElement, query } from 'lit/decorators.js';
-import anime, { AnimeInstance } from 'animejs/lib/anime.js';
 import { MainStyles } from '../../styles.js';
 import { ComponentStyles } from './splash-screen.styles.js';
+import * as THREE from 'three';
+import { GUI } from 'dat.gui'
+
 
 @customElement('splash-screen')
 export class SplashScreenComponent extends LitElement {
   static styles = [MainStyles, ComponentStyles];
 
-  bannerText = 'Ian Matson • ';
+  @query('.main-container') mainContainer!: HTMLElement;
+  @query('#canvas') canvas!: HTMLCanvasElement;
+  mouse: THREE.Vector2 = new THREE.Vector2(0, 0); 
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer();
+  mouseLight = new THREE.PointLight(0xffffff, 1);
+  raycaster = new THREE.Raycaster();
 
-  @query('.banner') banner!: HTMLDivElement;
+  constructor() {
+    super();
 
-  bannerAnimation: null | AnimeInstance = null;
-
-  resizeTimeout: number | null = null;
-
-  @query('.primary-photo') primaryPhoto!: HTMLDivElement;
-
-  connectedCallback() {
-    super.connectedCallback(); // eslint-disable-line
-
-    this.animateBannerText();
-    this.animatePrimaryPhoto();
-  }
-
-  animateBannerText() {
     this.updateComplete.then(() => {
-      this.initializeBannerAnimation();
-
-      window.onresize = () => {
-        if (this.resizeTimeout) {
-          clearTimeout(this.resizeTimeout);
-        }
-        this.resizeTimeout = window.setTimeout(() => {
-          console.log('resize');
-          this.initializeBannerAnimation();
-        }, 100);
-      };
+      this.threeJSInit();
     });
   }
+  
+  threeJSInit() {
+    this.renderer = new THREE.WebGLRenderer({ canvas: this.canvas });
+    this.camera.position.set(0, 0, 1);
+    this.renderer.setSize( window.innerWidth - 6, window.innerHeight - 6);
+    
+    const geometry = new THREE.BoxGeometry( 100, 100, 100 );
+    const material = new THREE.MeshStandardMaterial( { color: 0x333333 } );
+    const cube = new THREE.Mesh( geometry, material );
 
-  initializeBannerAnimation() {
-    this.banner.innerText = this.bannerText;
-    const initialWidth = this.banner.offsetWidth;
-    while (this.banner.offsetWidth < window.innerWidth + initialWidth) {
-      this.banner.innerText += this.bannerText;
-    }
-    if (this.bannerAnimation) {
-      anime.remove(this.banner);
-    }
-    this.bannerAnimation = anime({
-      targets: this.banner,
-      translateX: [0, -initialWidth],
-      duration: 7 * initialWidth,
-      easing: 'linear',
-      loop: true,
+
+    cube.position.set(0, 0, -51);
+    this.scene.add( cube );
+
+    const light = new THREE.DirectionalLight( 0xffffff, 4 );
+    light.position.set( 1, 1, 1 ).normalize();
+    this.scene.add( light );
+    this.scene.add(this.mouseLight);
+
+    window.addEventListener('mousemove', (event) => {
+      this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
     });
-  }
 
-  animatePrimaryPhoto() {
-    // Animates the background image to move in the reversed direction of the the user's cursor
-    this.updateComplete.then(() => {
-      window.addEventListener('mousemove', event => {
-        const x = (event.clientX / window.innerWidth) * 2 - 1;
-        const y = (event.clientY / window.innerHeight) * 2 - 1;
-        console.log(x, y);
+    window.onresize = () => {
+      this.renderer.setSize( window.innerWidth, window.innerHeight );
+      this.camera.aspect = window.innerWidth / window.innerHeight;
+      this.camera.updateProjectionMatrix();
+    };
+  
+    this.camera.position.z = 5;
+    const clock = new THREE.Clock();
+    const renderer = this.renderer;
+    const scene = this.scene;
+    const camera = this.camera;
+    const mouseLight = this.mouseLight;
+    const mouse = this.mouse;
+    const raycaster = this.raycaster;
+    const targetPosition = new THREE.Vector3();
+    function threeJSRender() {
+      const timeSinceLastFrame = clock.getDelta();
+      const elapsedTime = clock.getElapsedTime();
+      const lerpFactor = 1 - Math.exp(-5 * timeSinceLastFrame);
+      console.log(lerpFactor);
+      raycaster.setFromCamera(mouse, camera);
+      const planeZ = new THREE.Plane(new THREE.Vector3(0, 0, 1), .5);
+      const intersection = new THREE.Vector3();
+      raycaster.ray.intersectPlane(planeZ, intersection)!;
+      targetPosition.copy(intersection);
+      mouseLight.position.lerp(targetPosition, Math.min(1, lerpFactor * 2));
+      const distance = mouseLight.position.distanceTo(targetPosition);
+      mouseLight.intensity = distance * 3;
+      const color = new THREE.Color();
+      color.setHSL(Math.min(distance * .06, .5), 1, 0.5);
+      mouseLight.color = color;
 
-        anime.set(this.primaryPhoto, { scale: 1.04 });
-        anime({
-          targets: this.primaryPhoto,
-          translateX: `${-x * 2}%`,
-          translateY: `${-y * 2}%`,
-          duration: 500,
-          easing: 'easeOutQuad',
-        });
-      });
-    });
+      renderer.render( scene, camera ); 
+    }
+
+    renderer.setAnimationLoop( threeJSRender );
   }
 
   render() {
     return html`
-      <div class="main-container">
-        <div class="banner">${this.bannerText}</div>
-        <div class="primary-photo-container">
-          <img
-            class="primary-photo"
-            src="/assets/main-portrait.png"
-            alt="Ian Matson"
-          />
-          <div class="noise"></div>
-        </div>
-        <div class="arrow-text-container">
-          <div class="arrow-text">free-range software developer!</div>
-          <img class="arrow" src='assets/arrow.svg'/>
-        </div>
-        <div class="lower-text">
-          <p>Scroll down to learn more</p>
+      <div class='main-container'>
+        <canvas id="canvas"></canvas>
+        <div class = 'name-text'>
+          <h1>IAN<br>MAT<br>SON.</h1>
         </div>
       </div>
     `;
--- src/components/splash-screen/splash-screen.styles.ts
+++ src/components/splash-screen/splash-screen.styles.ts
@@ -3,122 +3,19 @@ import { css } from "lit";
 export const ComponentStyles = css`
   .main-container {
     display: flex;
-    flex-direction: column;
-    justify-content: stretch;
-    align-items: stretch;
-    height: 100vh;
+    justify-content: center;
+    align-items: center;
     width: 100vw;
+    height: 100vh;
     position: relative;
-    overflow-x: hidden;
-    background-color: var(--secondary-one);
-  }
-
-  .primary-photo-container {
-    flex-grow: 1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 50px;
-    margin-bottom: 0;
-    border-radius: 30px;
+    border: 3px solid #EAF0CE;
+    box-sizing: border-box;
     overflow: hidden;
-    position: relative;
   }
 
-  .primary-photo-container:after {
-    box-shadow: inset 0 4px 4px 0px rgba(0, 0, 0, 0.25);
-    content: '';
-    display: block;
-    height: 100%;
+  #canvas {
     position: absolute;
-    top: 0;
-    width: 100%;
-    border-radius: 30px;
-  }
-  
-  .primary-photo {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: right;
-    transform: scale(1.04);
-    z-index: 0;
-  }
-
-  .banner {
-    z-index: 1;
-    position: absolute;
-    top: 0;
-    left: 0;
-    color: var(--primary);
-    font-size: 6em;
-    font-family: var(--font-stack);
-    white-space: pre;
-  }
-
-  .banner:after {
-    /* This value is the OPPOSITE color of our background */
-    color: rgb(0, 255, 255); 
-    mix-blend-mode: difference;
-}
-
-  .noise {
-    position: absolute;
-    inset: -200%;
-    background-image: url('/assets/noise.png');
-    opacity: 0.05;
-
-    animation: shift .02s infinite linear both;
-    zoom: 3;
-  }
-
-  .arrow-text-container {
-    position: absolute;
-    left: 20%;
-    top: 20%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .arrow {
-    stroke: var(--accent);
-    width: 300px;
-    height: 300px;
-  }
-
-  .arrow-text {
-    transform: translateX(-100px) rotate(-10deg);
-    font-size: 1.5rem;
-  }
-
-  .lower-text {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  @keyframes shift { 
-    0% {
-      transform: translateX(10%) translateY(10%);
-    }
-
-    100% {
-      transform: translateX(-10%) translateY(-10%);
-    }
-  }
-
-  @media (max-width: 800px) {
-    .banner {
-      font-size: 4em;
-    }
-    .primary-photo-container {
-      margin: 20px;
-      margin-bottom: 0;
-    }
-
-    .main-container {
-      height: 400px;
-    }
+    top: 0px;
+    left: 0px;
   }
 `
