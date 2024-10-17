8a50ed1466c3ec5d35422245f7d2ed0cb86e62df
Ian <imatson9119@gmail.com>
2024-10-17 16:36:50 -0500

    Add more coffee mug textures and an order-based narrative mode

--- src/components/about-screen/about-screen.component.ts
+++ src/components/about-screen/about-screen.component.ts
@@ -4,7 +4,6 @@ import { MainStyles } from '../../styles.js';
 import { ComponentStyles } from './about-screen.styles.js';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { GUI } from 'dat.gui';
 
 const imageFaceNames = ['Cube001_2', 'Cube001_3', 'Cube001_4'];
 
@@ -14,6 +13,22 @@ export class AboutScreenComponent extends LitElement {
 
   @query('.main-container') mainContainer!: HTMLElement;
   @query('#canvas') canvas!: HTMLCanvasElement;
+
+  spinSpeed = 0.5;
+  storyMode = false;
+  startedStoryMode = false;
+  nTextures = 32;
+  curStoryTexture = 0;
+  nStoryTextures = 9;
+  mug: THREE.Object3D | null = null;
+  textures = Array.from({length: this.nTextures}, (_, i) => i + 1).map((i) => getMugTexture(`${i}`));
+  materials = [
+    new THREE.MeshStandardMaterial({color: 0xe6e3b3, flatShading: true, roughness: 0}),
+    new THREE.MeshStandardMaterial({color: 0xffffff, flatShading: true, roughness: 0, map: pickRandom(this.textures, this.nStoryTextures)}),
+    new THREE.MeshStandardMaterial({color: 0xffffff, flatShading: true, roughness: 0, map: pickRandom(this.textures, this.nStoryTextures)}),
+    new THREE.MeshStandardMaterial({color: 0xffffff, flatShading: true, roughness: 0, map: pickRandom(this.textures, this.nStoryTextures)}),
+  ];
+
 
   constructor() {
     super();
@@ -47,19 +62,11 @@ export class AboutScreenComponent extends LitElement {
   }
 
  animateMug(mug: THREE.Object3D, scene: THREE.Scene, camera: THREE.PerspectiveCamera, renderer: THREE.WebGLRenderer) {
-    const nTextures = 8;
-    const textures = Array.from({length: nTextures}, (_, i) => i + 1).map((i) => getMugTexture(`${i}`));
-    const materials = [
-      new THREE.MeshStandardMaterial({color: 0xe6e3b3, flatShading: true, roughness: 0}),
-      new THREE.MeshStandardMaterial({color: 0xffffff, flatShading: true, roughness: 0, map: pickRandom(textures)}),
-      new THREE.MeshStandardMaterial({color: 0xffffff, flatShading: true, roughness: 0, map: pickRandom(textures)}),
-      new THREE.MeshStandardMaterial({color: 0xffffff, flatShading: true, roughness: 0, map: pickRandom(textures)}),
-    ];
 
     let mousePos = [window.innerWidth/2, window.innerWidth/2];
     let cameraOffsetFactor = [.5, .5];
-    const flicker_lower_bound = 0.1;
-    const flicker_upper_bound = 0.4;
+    const flicker_lower_bound = 0.2;
+    const flicker_upper_bound = 0.5;
     const flicker_likelihood = 0.0015;
     const flickerShake = 1;
     let leftFace: THREE.Mesh | null = null;
@@ -75,23 +82,21 @@ export class AboutScreenComponent extends LitElement {
     mug.traverse((child) => {
       if (child instanceof THREE.Mesh) {
         const color = Math.floor(Math.random()*16777215);
-        console.log(child);
-        console.log(color);
         switch (child.name) {
           case 'Cube001_2':
             rightFace = child;
-            child.material = materials[3];
+            child.material = this.materials[3];
             break;
           case 'Cube001_3':
             centerFace = child;
-            child.material = materials[2];
+            child.material = this.materials[2];
             break;
           case 'Cube001_4':
             leftFace = child;
-            child.material = materials[1];
+            child.material = this.materials[1];
             break;
           default:
-            child.material = materials[0];
+            child.material = this.materials[0];
             break;
         }
       }
@@ -104,6 +109,7 @@ export class AboutScreenComponent extends LitElement {
     
 
     const clock = new THREE.Clock();
+    const ref = this;
 
     function animate() {
       const delta = clock.getDelta();
@@ -123,7 +129,7 @@ export class AboutScreenComponent extends LitElement {
           mug.position.x = 0;
           mug.position.y = 0;
           mug.position.z = -5;
-          materials.forEach((material, index) => {
+          ref.materials.forEach((material, index) => {
             index === 0 ? material.color.setHex(0xe6e3b3) : material.color.setHex(0xffffff);
             material.wireframe = false;
           });
@@ -134,18 +140,18 @@ export class AboutScreenComponent extends LitElement {
           const color = (r << 16) | (g << 8) | b;
 
   
-          materials.forEach((material) => {
+          ref.materials.forEach((material) => {
             material.color.setHex(color);
           });
           mug.position.x = Math.random() * flickerShake;
           mug.position.y = Math.random() * flickerShake;
           mug.position.z = -5 + Math.random() * flickerShake;
         }
-      } else if (Math.random() < flicker_likelihood) {
-        materials.forEach((material, index) => {
+      } else if (Math.random() < flicker_likelihood && ref.spinSpeed === .5 && !ref.storyMode) {
+        ref.materials.forEach((material, index) => {
           material.wireframe = true;
           if (index !== 0) {
-            material.map = pickRandom(textures);  
+            material.map = ref.getNextTexture();  
           }
         });
         flickerTime = Math.random() * (flicker_upper_bound - flicker_lower_bound) + flicker_lower_bound;
@@ -153,22 +159,35 @@ export class AboutScreenComponent extends LitElement {
 
       renderer.render( scene, camera );
       const rotation = mug.rotation.y / Math.PI % 2;
-      mug.rotation.y += .5 * delta;
+      mug.rotation.y += -1 * ref.spinSpeed * delta;
       const newRotation =  mug.rotation.y / Math.PI % 2;
 
-      if (newRotation < rotation) {
+      if (delta > 0 && ref.spinSpeed > .5){ 
+        ref.spinSpeed = Math.max(.5,  ref.spinSpeed * .99);
+      }
+
+      if (rotation < newRotation) {
         const material = (leftFace!.material as THREE.MeshStandardMaterial);
-        material.map = pickRandom(textures); 
+        material.map = ref.getNextTexture();
         material.needsUpdate = true;
-      } else if (rotation < 1.5 && newRotation > 1.5) {
+      } else if (rotation > -0.5 && newRotation < -0.5) {
         const material = (centerFace!.material as THREE.MeshStandardMaterial);
-        material.map = pickRandom(textures); 
+        material.map = ref.getNextTexture();
         material.needsUpdate = true;
-      } else if (rotation < 1 && newRotation > 1) {
+      } else if (rotation > -1 && newRotation < -1) {
         const material = (rightFace!.material as THREE.MeshStandardMaterial);
-        material.map = pickRandom(textures); 
+        material.map = ref.getNextTexture();
         material.needsUpdate = true;
       } 
+      
+      if (!ref.startedStoryMode && window.scrollY > 0) {
+        ref.storyMode = true;
+        ref.startedStoryMode = true;
+        mug.rotation.y = -0.5 * Math.PI;
+        (leftFace!.material as THREE.MeshStandardMaterial).map = ref.textures[0];
+        (centerFace!.material as THREE.MeshStandardMaterial).map = ref.textures[1];
+        ref.curStoryTexture = 2;
+      }
     }
     renderer.setAnimationLoop( animate );
 
@@ -177,7 +196,8 @@ export class AboutScreenComponent extends LitElement {
   init3JS() {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({canvas: this.canvas, alpha: true});
+    const renderer = new THREE.WebGLRenderer({canvas: this.canvas, alpha: true, antialias: true});
+    renderer.setPixelRatio(window.devicePixelRatio);
     const defaultCameraOffset = 10;
     const mobileCameraOffset = 10;
 
@@ -187,6 +207,7 @@ export class AboutScreenComponent extends LitElement {
     const x = this;
     loader.load( 'assets/mug.glb', function ( gltf ) {
       x.animateMug(gltf.scene, scene, camera, renderer);
+      x.mug = gltf.scene;
     }, undefined, function ( error ) {
       console.error( error );
     });
@@ -210,6 +231,23 @@ export class AboutScreenComponent extends LitElement {
     });
   }
 
+  boostSpin() {
+    this.spinSpeed = 30;
+  }
+
+  getNextTexture(): THREE.Texture {
+    if (this.storyMode) {
+      const ret = this.textures[this.curStoryTexture];
+      this.curStoryTexture++;
+      if (this.curStoryTexture >= this.nStoryTextures) {
+        this.storyMode = false;
+      }
+      return ret;
+    } else {
+      return pickRandom(this.textures, this.nStoryTextures);
+    }
+  }
+
   render() {
     return html`
       <div class='main-container'>
@@ -218,10 +256,10 @@ export class AboutScreenComponent extends LitElement {
             about me<span class='accent'>.</span>
           </div>
           <div class='body'>
-            <span>I'm a freelance <span class='accent'>software engineer</span> born and raised in <span class='accent'>Dallas, TX</span>. I'm always looking to <span class='accent'>learn</span>, and have a deep apprecitation for a <span class='accent'>good challenge</span>. Reach out and say hi!</span>
+            <span>I'm a freelance <span class='accent'>software engineer</span> born and raised in <span class='accent'>Dallas, TX</span>. I'm always looking to <span class='accent'>learn</span>, and have a deep appreciation for a <span class='accent'>good challenge</span>. Reach out and say hi!</span>
           </div>
         </div>
-        <canvas id='canvas'></canvas>
+        <canvas id='canvas' @click=${this.boostSpin}></canvas>
       </div>
       <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     `;
@@ -232,8 +270,8 @@ function isMobileViewport() {
   return window.innerWidth < 768;
 }
 
-function pickRandom<T>(array: T[]): T {
-  return array[Math.floor(Math.random() * array.length)];
+function pickRandom<T>(array: T[], low: number = 0, high: number = array.length): T {
+  return array[Math.floor(Math.random() * (high - low)) + low];
 }
 
 function getMugTexture(name: string) {
