baebab4712f6acdbbcbd0a4eb01a0b5be5ff630a
Ian <imatson9119@gmail.com>
2025-02-14 11:51:14 -0600

    Update local dev stack

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -1,26 +1,15 @@
-// import { hmrPlugin, presets } from '@open-wc/dev-server-hmr';
+import { esbuildPlugin } from '@web/dev-server-esbuild';
 
-/** Use Hot Module replacement by adding --hmr to the start command */
-const hmr = process.argv.includes('--hmr');
-
-export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
-  open: '/',
-  watch: !hmr,
-  /** Resolve bare module imports */
-  nodeResolve: {
-    exportConditions: ['browser', 'development'],
-  },
-  
-  /** Compile JS for older browsers. Requires @web/dev-server-esbuild plugin */
-  // esbuildTarget: 'auto'
-
-  /** Set appIndex to enable SPA routing */
-  appIndex: './index.html',
-
+export default {
+  rootDir: '.',
+  nodeResolve: true,
   plugins: [
-    /** Use Hot Module Replacement by uncommenting. Requires @open-wc/dev-server-hmr plugin */
-    // hmr && hmrPlugin({ exclude: ['**/*/node_modules/**/*'], presets: [presets.litElement] }),
+    esbuildPlugin({ 
+      ts: true,
+      target: 'es2020',
+      tsconfig: './tsconfig.json'
+    })
   ],
-
-  // See documentation for all available options
-});
+  appIndex: 'index.html',
+  open: true,
+};
